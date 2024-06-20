f2fa7c0c788116afdff5296c1ce61ce9acbc25ed
Anas-init <anastech900@gmail.com>
2024-06-20 06:23:48 -0700

    Minor changes

---
+++ myapp/src/components/dashboard.js
@@ -0,0 +1,11 @@
+import { useState,useEffect } from "react"
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import logo from './images/logo.png'
+const DashView = () => {
+    return (  
+        <div className="dash-class">
+        </div>
+    );
+}
+ 
+export default DashView;
