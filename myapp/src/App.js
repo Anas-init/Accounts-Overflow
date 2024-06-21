9bf1ba62bed5d62326d8389d2bebd19e7a9d1c9b
Anas-init <anastech900@gmail.com>
2024-06-21 13:32:33 -0700

    Issue pending

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,9 +1,8 @@
-import Example from "./components/navbar";
 
 function App() {
   return (
     <div className="App">
-      <Example/>
+      
     </div>
   );
 }
--- myapp/src/index.js
+++ myapp/src/index.js
@@ -3,14 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
  
-import { ThemeProvider } from "@material-tailwind/react";
- 
 const root = ReactDOM.createRoot(document.getElementById("root"));
  
 root.render(
-  <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+     <App />
 );
