7cc02252e58a963f0b6cd6b80407b952448b78e9
Anas-init <anastech900@gmail.com>
2024-06-28 14:41:57 -0700

    Minor fixes

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,9 +1,10 @@
 import Navbar from "./components/navbar";
-
+import { BrowserRouter as Switch,Route,Router } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <Navbar/>
+
     </div>
   );
 }
--- myapp/src/components/navbar.js
+++ myapp/src/components/navbar.js
@@ -1,67 +1,16 @@
 import React, { useState } from 'react';
-import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+import { AiOutlineClose, AiOutlineMenu, AiOutlineHome, AiOutlineTag, AiOutlineQuestionCircle } from 'react-icons/ai';
+import { FaStackOverflow, FaUser } from 'react-icons/fa';
+import {Link} from 'react-router-dom'
+
 
 const Navbar = () => {
-  // State to manage the navbar's visibility
-  const [nav, setNav] = useState(false);
-
-  // Toggle function to handle the navbar's display
-  const handleNav = () => {
-    setNav(!nav);
-  };
-
-  // Array containing navigation items
-  const navItems = [
-    { id: 1, text: 'Home' },
-    { id: 2, text: 'Company' },
-    { id: 3, text: 'Resources' },
-    { id: 4, text: 'About' },
-    { id: 5, text: 'Contact' },
-  ];
-
   return (
-    <div className='bg-black flex justify-between items-center h-16 max-w-[1240px] mx-auto px-4 text-white'>
-      {/* Logo */}
-      <h1 className='text-xl font-semibold text-[#00df9a]'>Accounts Overflow</h1>
-
-      {/* Desktop Navigation */}
-      <ul className='hidden md:flex text-sm'>
-        {navItems.map(item => (
-          <li
-            key={item.id}
-            className='px-3 py-2 hover:bg-[#00df9a] hover:text-black rounded-md m-1 cursor-pointer duration-300'
-          >
-            {item.text}
-          </li>
-        ))}
-      </ul>
-
-      {/* Mobile Navigation Icon */}
-      <div onClick={handleNav} className='block md:hidden'>
-        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+    <div className='Navbar-class'>
+      <div className='logo-class'>
+      <FaStackOverflow size={35} color='fb8500'></FaStackOverflow>
       </div>
-
-      {/* Mobile Navigation Menu */}
-      <ul
-        className={
-          nav
-            ? 'fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'
-            : 'ease-in-out w-[60%] duration-500 fixed top-0 left-[-100%]'
-        }
-      >
-        {/* Mobile Logo */}
-        <h1 className='text-xl font-semibold text-[#00df9a] m-4'>Accounts Overflow</h1>
-
-        {/* Mobile Navigation Items */}
-        {navItems.map(item => (
-          <li
-            key={item.id}
-            className='px-3 py-2 border-b hover:bg-[#00df9a] hover:text-black rounded-md duration-300 cursor-pointer border-gray-600'
-          >
-            {item.text}
-          </li>
-        ))}
-      </ul>
+      <span>Accounts Overflow.</span>
     </div>
   );
 };
