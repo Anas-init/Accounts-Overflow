2b81f0bbe133d89b9c421a440111eef550fc1bb3
Anas-init <anastech900@gmail.com>
2024-07-02 14:58:02 -0700

    Minor workings

--- myapp/src/components/navbar.js
+++ myapp/src/components/navbar.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import { AiOutlineClose, AiOutlineMenu, AiOutlineHome, AiOutlineTag, AiOutlineQuestionCircle } from 'react-icons/ai';
-import { FaStackOverflow, FaUser } from 'react-icons/fa';
+import { FaStackOverflow} from 'react-icons/fa';
 import {Link} from 'react-router-dom'
-
-
+import { CgProfile } from "react-icons/cg";
+import { GiTrophyCup } from "react-icons/gi";
+import { FcAbout } from "react-icons/fc";
 const Navbar = () => {
   return (
     <div className='Navbar-class'>
@@ -12,6 +12,11 @@ const Navbar = () => {
       </div>
       <span>Accounts Overflow.</span>
       <input type="text" placeholder='Search'/>
+      <div className='icon-class'>
+        <a href='#'><CgProfile size={20}/></a>
+        <a href='#'><GiTrophyCup size={20}/></a>
+        <a href='#'><FcAbout size={20}/></a>
+      </div>
     </div>
   );
 };
